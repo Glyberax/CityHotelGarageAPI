fdcc606a37a25b8d54717437e6045e2f53d414a6
Arda Çalışkan <alkanlardal@icloud.com>
2025-06-25 12:49:58 +0300

    Tam Katmanlı mimari

--- Repository/Interfaces/ICityRepository.cs
+++
@@ -1,9 +0,0 @@
-using CityHotelGarageAPI.Repository.Models;
-
-namespace CityHotelGarageAPI.Repository.Interfaces;
-
-public interface ICityRepository : IBaseRepository<City>
-{
-    IQueryable<City> GetCitiesWithHotels();
-    Task<City?> GetCityWithHotelsAsync(int id);
-}
